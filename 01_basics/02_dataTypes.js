491def9ffd32bcc577e981bf3bec472e80c56977
Nikul Boharwal <75658868+nikul1711@users.noreply.github.com>
2024-04-09 14:46:34 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict" //treat all js code as new version
+
+let name = "Nikul"
+let age = 13
+let isLoggedIn=false
+
+// all DataTypes
+/* 
+
+int= use for the number
+string = store for the text
+boolean = store value true or false
+null= stand alone variable
+undefined =>
+symbol => unique
+
+
+
+*/
+
+console.log(typeof undefined); // undefined is a type
+console.log(typeof null); // object
