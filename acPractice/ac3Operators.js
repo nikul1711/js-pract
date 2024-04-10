8966dfe40ab7fbea724f89d15d74afc5fb685601
Nikul Boharwal <75658868+nikul1711@users.noreply.github.com>
2024-04-10 15:51:32 +0000

    AC Operators

---
+++ acPractice/ac3Operators.js
@@ -0,0 +1,10 @@
+console.log("----------------------------- Operators -----------------------------")
+
+let a =100
+let b=15
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
