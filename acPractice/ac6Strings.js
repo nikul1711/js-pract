774b968731921b44f649979b2547d9c05e4a0867
Nikul Boharwal <75658868+nikul1711@users.noreply.github.com>
2024-04-24 07:56:28 +0000

    String Practice

--- acPractice/ac6Strings.js
+++ acPractice/ac6Strings.js
@@ -1,3 +1,22 @@
 console.log("----------------------------------------------- String Concatinations -----------------------------------------------")
 
-let value="adsf"
+let value="Hey Nikul How are you, I hope you doing well!"
+let str=value.toUpperCase()
+console.log(str);
+
+ str=value.toLowerCase().trim()
+ console.log(str);
+ console.log(str.charAt(1));
+
+ for (let index = 0; index < str.length; index++) {
+    console.log(str.charAt(index));
+ }
+
+ let v1="hello"
+ let v2="hy"
+ let v3=v1.concat(v2)
+ console.log(v3);
+
+
+ let userName="nikul"
+ console.log("@"+userName+userName.length);
