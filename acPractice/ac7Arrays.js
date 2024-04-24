c98a66b523415b8258ee148995c2cf35b16147b9
Nikul Boharwal <75658868+nikul1711@users.noreply.github.com>
2024-04-24 09:48:20 +0000

    arrays

---
+++ acPractice/ac7Arrays.js
@@ -0,0 +1,10 @@
+// Arrays in JS (Collection of items)
+let heroes=["Ironman","Captain America","Thor","Loki","Spiderman"]
+let marks=[11,15,12,14,13]
+let info=["rahul",89,"Delhi"]
+
+
+ for (let index = 0; index < heroes.length; index++) {
+    const element = heroes[index];
+    console.log(element);
+ }
